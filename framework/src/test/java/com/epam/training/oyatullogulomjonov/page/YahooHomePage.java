b6924c06de708ac850c274231c7dffe2b48358fd
Oyatullo Gulomjonov <gulomjonovoyatullo@gmail.com>
2023-06-02 10:44:03 +0500

    framework YahooHomePage updated

--- framework/src/test/java/com/epam/training/oyatullogulomjonov/page/YahooHomePage.java
+++ framework/src/test/java/com/epam/training/oyatullogulomjonov/page/YahooHomePage.java
@@ -2,7 +2,6 @@ package com.epam.training.oyatullogulomjonov.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
 
 public class YahooHomePage extends AbstractPage {
     private By mailButtonBy = By.id("ybar-navigation-item-mail");
