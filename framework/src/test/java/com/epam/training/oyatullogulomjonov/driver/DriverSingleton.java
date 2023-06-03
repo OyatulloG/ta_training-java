801b80cf4f23c0853c01b6074e33c172b19fde31
Oyatullo Gulomjonov <gulomjonovoyatullo@gmail.com>
2023-06-04 01:49:08 +0500

    framework chromeOptions update

--- framework/src/test/java/com/epam/training/oyatullogulomjonov/driver/DriverSingleton.java
+++ framework/src/test/java/com/epam/training/oyatullogulomjonov/driver/DriverSingleton.java
@@ -26,7 +26,7 @@ public class DriverSingleton {
             chromeOptions.addArguments("--remote-allow-origins=*");
             chromeOptions.addArguments("--window-size=1920,1080");            
             chromeOptions.addArguments("--allow-insecure-localhost");
-            //chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--headless");
             chromeOptions.addArguments("--disable-gpu");
             chromeOptions.addArguments("--no-sandbox");
             chromeOptions.addArguments("--ignore-certificate-errors");            
