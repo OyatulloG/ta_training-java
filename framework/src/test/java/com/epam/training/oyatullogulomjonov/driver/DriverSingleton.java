796c9b936f000d11f21c62b18bdab902414e305d
Oyatullo Gulomjonov <gulomjonovoyatullo@gmail.com>
2023-06-04 15:27:02 +0500

    framework firefox driver

--- framework/src/test/java/com/epam/training/oyatullogulomjonov/driver/DriverSingleton.java
+++ framework/src/test/java/com/epam/training/oyatullogulomjonov/driver/DriverSingleton.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverSingleton {
     private static WebDriver driver;
@@ -17,7 +18,9 @@ public class DriverSingleton {
         switch (System.getProperty("browser")){
           case "firefox": {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--headless");
+            driver = new FirefoxDriver(firefoxOptions);
             break;
           }
           default: {
